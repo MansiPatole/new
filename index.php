83467be0711b99d03f61447a691ac80d07dece36
MansiPatole <45715877+MansiPatole@users.noreply.github.com>
2019-08-04 15:15:03 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<title>Banner</title>
+	<link href="css/style.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Great+Vibes&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Great+Vibes|Niconne&display=swap" rel="stylesheet">
+</head>
+
+<body>
+	<section class="banner">
+
+		<div class="heading-area">
+			<h1>
+				This is heading.
+			</h1>
+			<p>
+				Write your content here.
+			</p>
+			
+			<button>Click Here</button>
+		</div>
+	</section>
+
+<section class="part-2"></section>
+</body>
+
+</html>
